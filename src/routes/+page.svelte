de66c24a404aaf67815b154edaf8dd30f293725e
SimonQuerrioux <sim.querrioux@gmail.com>
2025-05-07 19:04:21 +0200

    7.5.2025

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,41 @@
 <script lang="ts">
 	let response = '';
-	let input = '';
+	let message = '';
 	let loading = false;
-
-	let type: 'joke' | 'image' = 'joke';
+	let history: [string, string][] = [];
 
 	async function handleSubmit() {
+		console.log(message);
 		loading = true;
-		const result = await fetch('/api/' + type, {
-			method: 'POST',
-			body: input
-		});
-		const data = await result.json();
 
+		history = [...history, ['human', message]];
+		message = '';
+
+		const response = await fetch('/api/chat', {
+			method: 'POST',
+			body: JSON.stringify({ history })
+		});
+		const data = await response.json();
+
+		history = [...history, ['assistant', data.message]];
 		loading = false;
-		input = '';
-		response = data.message;
 	}
 </script>
 
 <main class="flex flex-col items-center justify-center h-screen">
-	<div class="w-full p-4 text-center">
-		{response}
-	</div>
+	{#each history as h}
+		<div
+			class="rounded-xl {h[0] == 'human'
+				? 'bg-slate-300 py-1 px-3 m-1 float-right'
+				: 'bg-blue-500 py-1 px-3 m-1 text-white float-left'}"
+		>
+			{h[1]}
+		</div>
+	{/each}
 
 	<form on:submit={handleSubmit} class="flex gap-2 max-w-md mx-auto mt-4">
-		<select bind:value={type}>
-			<option value="joke">Joke</option>
-			<option value="image">Image</option>
-		</select>
 		<input
-			bind:value={input}
+			bind:value={message}
 			class="flex-1 px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 w-96"
 			placeholder="Type your message..."
 		/>
